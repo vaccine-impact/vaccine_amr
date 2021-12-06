7b950b079b6d9371ddefe172d40ab83e3856b85e
Kaja A <kaja_a@yahoo.com>
2021-12-06 17:12:11 +0900

    main program

--- code/functions.R
+++ code/functions.R
@@ -8,7 +8,11 @@
 # ------------------------------------------------------------------------------
 # create data table of (194) WHO countries classified by (6) WHO regions 
 # and (4) World Bank income levels
-create_country_table <- function () {
+create_country_table <- function (regional_classification_file,
+                                  country_income_classification_file) {
+  
+  # open files for WHO country-region and WB country-income classifications
+  regional_dt <- fread (file = regional_classification_file)
   
   
 } # end of function -- create_country_table
--- code/main_program.R
+++ code/main_program.R
@@ -4,6 +4,10 @@
 # analysis code to estimate vaccine averted AMR health burden
 # ------------------------------------------------------------------------------
 
+# load libraries
+library (countrycode)         # country codes
+library (data.table)          # data table
+
 # remove all objects from workspace
 rm (list = ls ())
 
@@ -18,16 +22,22 @@ source ("functions.R")
 source_wd <- getwd ()
 setwd ("../")
 
-# # create data table of (194) WHO countries classified by (6) WHO regions 
+# ------------------------------------------------------------------------------
+
+# create data table of (194) WHO countries classified by (6) WHO regions 
 # and (4) World Bank income levels
-create_country_table ()
+regional_classification_file       <- file.path ("data", "regional_classification.csv")
+country_income_classification_file <- file.path ("data", "country_income_classification.csv")
+create_country_table (regional_classification_file,
+                      country_income_classification_file)
+
+# ------------------------------------------------------------------------------
 
 # return to source directory
 setwd (source_wd)
 
-
 # end time
 end_time <- Sys.time ()
-print (paste0 ("end time = ", start_time))
+print (paste0 ("end time = ", end_time))
 print (Sys.time () - start_time)
 # ------------------------------------------------------------------------------
