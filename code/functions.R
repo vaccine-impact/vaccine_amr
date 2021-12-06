471d83643fa27a78a70217d23e679f61970f5383
Kaja A <kaja_a@yahoo.com>
2021-12-06 17:03:40 +0900

    file format

--- code/functions.R
+++ code/functions.R
@@ -3,3 +3,13 @@
 #
 # functions for analysis to estimate vaccine averted AMR health burden
 # ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+# create data table of (194) WHO countries classified by (6) WHO regions 
+# and (4) World Bank income levels
+create_country_table <- function () {
+  
+  
+} # end of function -- create_country_table
+# ------------------------------------------------------------------------------
--- code/mian_program.R
+++ code/mian_program.R
@@ -4,3 +4,30 @@
 # analysis code to estimate vaccine averted AMR health burden
 # ------------------------------------------------------------------------------
 
+# remove all objects from workspace
+rm (list = ls ())
+
+# start time
+start_time <- Sys.time ()
+print (paste0 ("start time = ", start_time))
+
+# source functions
+source ("functions.R")
+
+# move to base directory (run code from source directory)
+source_wd <- getwd ()
+setwd ("../")
+
+# # create data table of (194) WHO countries classified by (6) WHO regions 
+# and (4) World Bank income levels
+create_country_table ()
+
+# return to source directory
+setwd (source_wd)
+
+
+# end time
+end_time <- Sys.time ()
+print (paste0 ("end time = ", start_time))
+print (Sys.time () - start_time)
+# ------------------------------------------------------------------------------
