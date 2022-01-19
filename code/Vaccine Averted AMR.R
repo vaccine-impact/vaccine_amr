24001e7375a5f17d37f7afd01f0ffb99c236e52d
ckim0509 <ckim0509@gmail.com>
2022-01-19 22:56:17 +0900

    GAS

--- code/Vaccine Averted AMR.R
+++ code/Vaccine Averted AMR.R
@@ -3,11 +3,12 @@
 # ------------------------------------------------------------------------------
 # load libraries
 library (readr)
-library(readxl)
+library (readxl)
 library (dplyr)
 library (ggplot2)
 library (reshape2)
 library (tidyverse)
+library (data.table)
 
 # clear workspace
 rm (list = ls ())
@@ -39,39 +40,74 @@ IHME_AMR_burden$Age_group <- factor(IHME_AMR_burden$Age_group,
                                 levels=c(as.vector(unlist(IHME_AMR_burden[1:23,"Age_group"]))), order=T)
 # ------------------------------------------------------------------------------
 # Import WHO vaccine profile
+Vaccine_profile <- read_excel("Vaccine profile for IHME burden.xlsx", 
+                              sheet = "Vaccine profile assumptions")
 
+Vaccine_profile <- Vaccine_profile[,c(3, 4, 5, 6, 9, 10)]
 
+Vaccine_profile <- Vaccine_profile %>%
+  rename("Efficacy" = "Efficacy (%)",
+         "Coverage" = "Coverage in target group",
+         "Duration" = "Duration of protection (year)", 
+         "DiseasePresentation" = "Disease presentation")
 
+Vaccine_profile$Efficacy <- Vaccine_profile$Efficacy * 1/100
+Vaccine_profile$Coverage <- Vaccine_profile$Coverage * 1/100
+
+fwrite (x    = Vaccine_profile, 
+        file = "Vaccine_profile.csv")
 # ------------------------------------------------------------------------------
+# Key element vectors
+
 reference <- IHME_AMR_burden %>% count(Pathogen, Disease_presentation)
 reference
 
-pathogen <- IHME_AMR_burden %>% count(Pathogen)
-pathogen
+pathogenlist <- IHME_AMR_burden %>% count(Pathogen)
+pathogenlist <- as.vector(unlist(pathogenlist[,"Pathogen"]))
 
-plot <- function(df, pathogen){
+burden <- IHME_AMR_burden %>% count(Disease_presentation)
+burden <- as.vector(unlist(burden[,"Disease_presentation"]))
+
+WHO_region <- IHME_AMR_burden %>% count(WHO_region)
+WHO_region <- as.vector(unlist(WHO_region[,"WHO_region"]))
+
+burden_type <- names(IHME_AMR_burden)[5:13]
+# ------------------------------------------------------------------------------
+##"Group A Streptococcus"
+
+Group_A_Streptococcus <- IHME_AMR_burden[IHME_AMR_burden$Pathogen == "Group A Streptococcus",]
+
+Group_A_Streptococcus <- left_join(Group_A_Streptococcus, Vaccine_profile, by=c("Pathogen" = "Pathogen"))
+
+Group_A_Streptococcus <- Group_A_Streptococcus %>% 
+  group_by(WHO_region, Pathogen, Efficacy, Coverage, Age_group) %>%
+  summarise(sum_Attributable_resistance_mean=sum(Attributable_resistance_mean), .groups = 'drop') 
+
+Group_A_Streptococcus <- Group_A_Streptococcus %>% 
+  mutate(v_Attributable_resistance_mean = ifelse(Age_group >= "Post Neonatal" & Age_group < "5 to 9",
+                                                 sum_Attributable_resistance_mean * Efficacy * Coverage,
+                                                 sum_Attributable_resistance_mean),
+        va_Attributable_resistance_mean = v_Attributable_resistance_mean-sum_Attributable_resistance_mean)
+
+burdenplot <- function(region) {
   
-  y <- df[df$Pathogen == pathogen,]
-  
-  y <- y %>% 
-    group_by(Pathogen, Age_group) %>%
-    summarise(sum=sum(Attributable_resistance_mean))
-    
-  ggplot(y, aes(x=Age_group)) +
-    geom_line(aes(y=sum, group=1, colour="vaccinated")) +
-    ylab("Number of Vaccine Averted Deaths") + 
-    xlab("Age") +
-    ggtitle(pathogen) +
-    theme_bw() +
-    theme(plot.title = element_text(hjust = 0.5)) +
-    expand_limits(y=0)
-  
-  }
+y  <- Group_A_Streptococcus[Group_A_Streptococcus$WHO_region == region,] 
 
-plot(df = IHME_AMR_burden, pathogen = "Group A Streptococcus")
+ggplot(y, aes(x=Age_group)) +
+  geom_line(aes(y=sum_Attributable_resistance_mean, group=1, colour="non-vaccinated")) +
+  geom_line(aes(y=v_Attributable_resistance_mean, group=1, colour="vaccinated")) +
+  ylab("Number of Vaccine Attributable Deaths") + 
+  xlab("Age") +
+  ggtitle(paste("Group A Streptococcus","(", region,")")) +
+  theme_bw() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  expand_limits(y=0)
+}
 
+lapply(WHO_region, burdenplot)
 
+Vaccine_averted_burden <- Group_A_Streptococcus %>%
+  group_by(WHO_region) %>%
+  summarise(averted_burden=sum(sum_Attributable_resistance_mean))
 
-
-
-
+# ------------------------------------------------------------------------------
