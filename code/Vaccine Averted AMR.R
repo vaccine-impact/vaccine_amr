0f7f2847056e31b633c3cb7c5c7914eebf37b10f
ckim0509 <ckim0509@gmail.com>
2022-01-23 21:59:11 +0900

    Vaccine averted deaths

--- code/Vaccine Averted AMR.R
+++ code/Vaccine Averted AMR.R
@@ -12,6 +12,9 @@ library (data.table)
 
 # clear workspace
 rm (list = ls ())
+
+# set working directory
+setwd("~/GitHub/vaccine_amr/data")
 # ------------------------------------------------------------------------------
 ## IHME data on the burden ##
 # WHO region: Africa, Americas, Eastern Mediterranean, Europe, South-East Asia, 
@@ -23,7 +26,7 @@ rm (list = ls ())
 
 IHME_AMR_burden <- read_excel("IHME AMR burden.xlsx", 
                               col_names = FALSE)
-
+# Data cleaning
 names(IHME_AMR_burden) <- c("WHO_region", "Disease_presentation","Age_group","Pathogen", 
                             "Associated_suscetible_mean","Associated_suscetible_lower",
                             "Associated_suscetible_upper","Associated_resistant_mean", 
@@ -31,19 +34,25 @@ names(IHME_AMR_burden) <- c("WHO_region", "Disease_presentation","Age_group","Pa
                             "Attributable_resistance_mean","Attributable_resistance_lower", 
                             "Attributable_resistance_upper")
 
-# Data cleaning
+
 IHME_AMR_burden <- IHME_AMR_burden[-c(1:2),]
 
 IHME_AMR_burden[,5:13] <- lapply(IHME_AMR_burden[,5:13], as.numeric)
 
 IHME_AMR_burden$Age_group <- factor(IHME_AMR_burden$Age_group, 
                                 levels=c(as.vector(unlist(IHME_AMR_burden[1:23,"Age_group"]))), order=T)
+
+levels(IHME_AMR_burden$Age_group) <- c("EN", "LN", "PN",  "1 to 4", "5 to 9","10 to 14",
+                                       "15 to 19", "20 to 24"  ,"25 to 29",  "30 to 34",
+                                       "35 to 39", "40 to 44", "45 to 49",  "50 to 54" ,      
+                                       "55 to 59", "60 to 64", "65 to 69", "70 to 74",
+                                       "75 to 79", "80 to 84", "85 to 89", "90 to 94", "95 plus")      
 # ------------------------------------------------------------------------------
 # Import WHO vaccine profile
 Vaccine_profile <- read_excel("Vaccine profile for IHME burden.xlsx", 
                               sheet = "Vaccine profile assumptions")
 
-Vaccine_profile <- Vaccine_profile[,c(3, 4, 5, 6, 9, 10)]
+Vaccine_profile <- Vaccine_profile[,c(3, 4, 5, 6, 9, 10,12)]
 
 Vaccine_profile <- Vaccine_profile %>%
   rename("Efficacy" = "Efficacy (%)",
@@ -53,6 +62,8 @@ Vaccine_profile <- Vaccine_profile %>%
 
 Vaccine_profile$Efficacy <- Vaccine_profile$Efficacy * 1/100
 Vaccine_profile$Coverage <- Vaccine_profile$Coverage * 1/100
+
+Vaccine_profile <- Vaccine_profile %>% filter(Selection == "Yes")
 
 fwrite (x    = Vaccine_profile, 
         file = "Vaccine_profile.csv")
@@ -73,41 +84,121 @@ WHO_region <- as.vector(unlist(WHO_region[,"WHO_region"]))
 
 burden_type <- names(IHME_AMR_burden)[5:13]
 # ------------------------------------------------------------------------------
-##"Group A Streptococcus"
 
-Group_A_Streptococcus <- IHME_AMR_burden[IHME_AMR_burden$Pathogen == "Group A Streptococcus",]
+## the death trend across all age groups
 
-Group_A_Streptococcus <- left_join(Group_A_Streptococcus, Vaccine_profile, by=c("Pathogen" = "Pathogen"))
-
-Group_A_Streptococcus <- Group_A_Streptococcus %>% 
-  group_by(WHO_region, Pathogen, Efficacy, Coverage, Age_group) %>%
-  summarise(sum_Attributable_resistance_mean=sum(Attributable_resistance_mean), .groups = 'drop') 
-
-Group_A_Streptococcus <- Group_A_Streptococcus %>% 
-  mutate(v_Attributable_resistance_mean = ifelse(Age_group >= "Post Neonatal" & Age_group < "5 to 9",
-                                                 sum_Attributable_resistance_mean * Efficacy * Coverage,
-                                                 sum_Attributable_resistance_mean),
-        va_Attributable_resistance_mean = v_Attributable_resistance_mean-sum_Attributable_resistance_mean)
-
-burdenplot <- function(region) {
+GlobalTrend <- function(pathogen){
+  x <- IHME_AMR_burden[IHME_AMR_burden$Pathogen == pathogen,]
   
-y  <- Group_A_Streptococcus[Group_A_Streptococcus$WHO_region == region,] 
-
-ggplot(y, aes(x=Age_group)) +
-  geom_line(aes(y=sum_Attributable_resistance_mean, group=1, colour="non-vaccinated")) +
-  geom_line(aes(y=v_Attributable_resistance_mean, group=1, colour="vaccinated")) +
-  ylab("Number of Vaccine Attributable Deaths") + 
-  xlab("Age") +
-  ggtitle(paste("Group A Streptococcus","(", region,")")) +
-  theme_bw() +
-  theme(plot.title = element_text(hjust = 0.5)) +
-  expand_limits(y=0)
+  x <- left_join(x, Vaccine_profile, by=c("Pathogen" = "Pathogen"))
+  
+  x <- x %>% 
+    group_by(Pathogen, Efficacy, Coverage, Age_group) %>%
+    summarise(sum_Attributable_resistance_mean=sum(Attributable_resistance_mean), .groups = 'drop') 
+  
+  x <- x %>% 
+    mutate(v_Attributable_resistance_mean = sum_Attributable_resistance_mean * Efficacy * Coverage,
+           va_Attributable_resistance_mean = v_Attributable_resistance_mean-sum_Attributable_resistance_mean)
+  
+  ggplot(x, aes(x=Age_group)) +
+    geom_line(aes(y=sum_Attributable_resistance_mean, group=1, colour="non-vaccinated")) +
+    ylab("Number of Vaccine Attributable Deaths") + 
+    xlab("Age") +
+    ggtitle(paste(pathogen,"(global)")) +
+    theme_bw() +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    expand_limits(y=0)
+  
+  # save plot
+  ggsave (filename = paste(pathogen,"_global.png"),
+          width = 15, 
+          height = 6, 
+          dpi = 600
+          )
 }
 
-lapply(WHO_region, burdenplot)
-
-Vaccine_averted_burden <- Group_A_Streptococcus %>%
-  group_by(WHO_region) %>%
-  summarise(averted_burden=sum(sum_Attributable_resistance_mean))
+lapply(pathogenlist, GlobalTrend)
 
 # ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+
+burden_vaccine_profile <- left_join(IHME_AMR_burden, Vaccine_profile, by=c("Pathogen" = "Pathogen"))
+  
+burden_vaccine_profile <- burden_vaccine_profile %>%
+    mutate(v_Attributable_resistance_mean 
+           = ifelse(Vaccination=="4 week (effective at 6 week)" & Duration=="5" & Age_group == "Post Neonatal", Attributable_resistance_mean * Efficacy * Coverage * 47/48,
+                    ifelse(Vaccination=="4 week (effective at 6 week)" & Duration=="5" & Age_group == "1 to 4", Attributable_resistance_mean * Efficacy * Coverage,
+                    ifelse(Vaccination=="4 week (effective at 6 week)" & Duration=="5" & Age_group == "5 to 9", Attributable_resistance_mean * Efficacy * Coverage * 1/5*5/52,
+                           
+             ifelse(Vaccination=="4 week (effective at 6 week)" & Duration=="2" & Age_group == "Post Neonatal", Attributable_resistance_mean * Efficacy * Coverage * 47/48,
+                    ifelse(Vaccination=="4 week (effective at 6 week)" & Duration=="2" & Age_group == "1 to 4", Attributable_resistance_mean * Efficacy * Coverage * 1/4*5/52,
+                           
+             ifelse(Vaccination=="4 week (effective at 6 week), 60 or above" & Duration=="2" & Age_group == "Post Neonatal", Attributable_resistance_mean * Efficacy * Coverage * 47/48,
+                    ifelse(Vaccination=="4 week (effective at 6 week), 60 or above" & Duration=="2" & Age_group == "1 to 4", Attributable_resistance_mean * Efficacy * Coverage * 1/4*5/52,
+                    ifelse(Vaccination=="4 week (effective at 6 week), 60 or above" & Duration=="2" & Age_group == "60 to 64", Attributable_resistance_mean * Efficacy * Coverage * (52+5)/(52*5),
+
+              ifelse(Vaccination=="12 weeks" & Duration=="20" & Age_group == "Post Neonatal", Attributable_resistance_mean * Efficacy * Coverage * 39/48,
+                                  ifelse(Vaccination=="12 weeks" & Duration=="20" & Age_group == "1 to 4", Attributable_resistance_mean * Efficacy * Coverage,
+                                         ifelse(Vaccination=="12 weeks" & Duration=="20" & Age_group == "5 to 9", Attributable_resistance_mean * Efficacy * Coverage,
+                                                ifelse(Vaccination=="12 weeks" & Duration=="20" & Age_group == "10 to 14", Attributable_resistance_mean * Efficacy * Coverage,
+                                                       ifelse(Vaccination=="12 weeks" & Duration=="20" & Age_group == "15 to 19", Attributable_resistance_mean * Efficacy * Coverage,
+                                                              ifelse(Vaccination=="12 weeks" & Duration=="20" & Age_group == "20 to 24", Attributable_resistance_mean * Efficacy * Coverage * 1/5 * 13/52,
+                                                                     
+             ifelse(Vaccination=="10 or above" & Duration=="10" & Age_group == "10 to 14", Attributable_resistance_mean * Efficacy * Coverage,
+                    ifelse(Vaccination=="10 or above" & Duration=="10" & Age_group == "15 to 19", Attributable_resistance_mean * Efficacy * Coverage,
+                                         
+             ifelse(Vaccination=="60 or above" & Duration=="10" & Age_group == "60 to 64", Attributable_resistance_mean * Efficacy * Coverage,
+                    ifelse(Vaccination=="60 or above" & Duration=="10" & Age_group == "65 to 69", Attributable_resistance_mean * Efficacy * Coverage,
+                           
+             ifelse(Vaccination=="60 or above" & Duration=="5" & Age_group == "60 to 64", Attributable_resistance_mean * Efficacy * Coverage,
+                    
+             ifelse(Vaccination=="60 or above" & Duration=="2" & Age_group == "60 to 64", Attributable_resistance_mean * Efficacy * Coverage * 2/5,
+                    
+             ifelse(Vaccination=="60 or above" & Duration=="1" & Age_group == "60 to 64", Attributable_resistance_mean * Efficacy * Coverage * 1/5,
+ 
+                        Attributable_resistance_mean))))))))))))))))))))),
+             va_Attributable_resistance_mean = Attributable_resistance_mean - v_Attributable_resistance_mean)
+  
+BurdenAverted <- function(pathogen){
+  x <- burden_vaccine_profile[burden_vaccine_profile$Pathogen == pathogen,]
+  
+  x <- x %>%
+    group_by(WHO_region) %>%
+    summarise(averted_burden=sum(va_Attributable_resistance_mean), .groups = 'drop')
+
+  x <- x[, 2]
+
+  colnames(x) <- pathogen
+
+  return(x)}
+
+BurdenAverted_df <- lapply(pathogenlist, BurdenAverted)
+
+BurdenAverted_df <- do.call(cbind.data.frame, BurdenAverted_df)
+
+BurdenAverted_df$WHO_region <- WHO_region
+# ------------------------------------------------------------------------------
+# Table 2
+Death_Averted <- BurdenAverted_df %>% 
+  mutate(sum = rowSums(.[1:14]))
+
+Death_Averted <- Death_Averted[-6,c("WHO_region", "sum")]
+
+fwrite (x    = Death_Averted,
+        file = "Death.Averted.csv")
+
+# ------------------------------------------------------------------------------
+# Figure 1
+ggplot(Death_Averted) +
+  aes(x = WHO_region, weight = sum) +
+  geom_bar(fill = "#8DA5B6") +
+  labs(x = "WHO region", y = "Vaccine Averted Deaths") +
+  theme_classic()
+
+# ------------------------------------------------------------------------------
+# Figure 2
+
+# ------------------------------------------------------------------------------
+# Figure 3
